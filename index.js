e0fec32fd5bff22be3c639c872829506e4473876
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2022-02-18 17:37:35 +0900

    equipment 수정 mutaion 생성

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const typeDefs = gql`
       count: Int
       new_or_used: String
     ): Equipment
+    editEquipment(
+      id: String
+      used_by: String
+      count: Int
+      new_or_used: String
+    ): Equipment
   }
   type Team {
     id: Int
@@ -66,6 +72,16 @@ const resolvers = {
     insertEquipment: (parent, args, context, info) => {
       database.equipments.push(args);
       return args;
+    },
+    editEquipment: (parent, args, context, info) => {
+      return database.equipments
+        .filter((equipment) => {
+          return equipment.id === args.id;
+        })
+        .map((equipment) => {
+          Object.assign(equipment, args);
+          return equipment;
+        })[0];
     },
   },
 };
